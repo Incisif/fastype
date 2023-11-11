4653d489ce51000e741a943f575b6c7db2acb20d
incisif <loeildejack@gmail.com>
2023-11-11 02:08:56 +0100

    -Dynamic Line Breaks: Lines are now determined by the available width, ensuring that text wraps properly within the container. -Smooth Scrolling: When typing, the view smoothly scrolls down to reveal the next line of text, improving user experience. -Optimized Character Styling: Characters are color-coded based on correctness, enhancing visual feedback. -Handling Dead Keys: Support for dead keys is added, allowing users to input accented characters. -Responsive Layout: The component adapts to the container's width, making it more responsive to different screen sizes. -Improved Code Structure: The code is organized for better readability and maintainability.

--- fastype/src/components/TypingBox/index.tsx
+++ fastype/src/components/TypingBox/index.tsx
@@ -1,40 +1,72 @@
-import React, { useEffect, useState, useRef } from "react";
+import styled from "styled-components";
+
+import React, {
+  useEffect,
+  useState,
+  useRef,
+  useMemo,
+  useCallback,
+} from "react";
 import { useSelector } from "react-redux";
-import { useAppDispatch } from "../../store/store";
+import { RootState, useAppDispatch } from "../../store/store";
 import { fetchTexts } from "../../features/text/textThunk";
 import { selectText } from "../../features/text/textSlice";
-import styled from "styled-components";
-import { v4 as uuidv4 } from "uuid";
-
 interface CharBoxProps {
   $status: string | null;
+  $lineIndex: number;
+  $wordIndex: number;
+  $charIndex: number;
+}
+interface WordWithIndex {
+  word: string;
+  startIndex: number;
 }
 
+interface LineType {
+  line: WordWithIndex[];
+  lineIndex: number;
+}
+
+interface CharStatus {
+  [key: number]: string;
+}
+
+interface TextContainerProps {
+  $translateY: number;
+}
 const TypingBoxContainer = styled.div`
   font-family: "Roboto", sans-serif;
   font-size: 1.5rem;
   display: flex;
   flex-wrap: wrap;
   width: 80%;
-  height: 400px;
+  height: 410px;
   background-color: #faf8f8;
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-  margin: 2rem 0;
-  padding: 1rem;
+  margin: 2rem 0 1rem 0;
+  padding: 1.2rem;
   overflow: hidden;
+  outline: none;
+  border-radius: 15px;
+`;
+const TextContainer = styled.div<TextContainerProps>`
+  width: 100%;
+  transform: translateY(-${(props) => props.$translateY}px);
+  transition: transform 0.3s ease; // Ajoutez une transition pour un effet de défilement fluide
 `;
 
+const LineContainer = styled.span`
+  display: flex;
+  margin-bottom: 1.5rem;
+`;
 const WordContainer = styled.div`
   display: flex;
-  white-space: nowrap;
-  line-height: 1;
-  margin-bottom: 2rem;
   height: 25px;
 `;
 
 const CharBox = styled.div<CharBoxProps>`
   display: flex;
-  width: 20px;
+  width: 18px;
   margin: 0 1px;
   text-align: center;
   align-items: center;
@@ -44,157 +76,243 @@ const CharBox = styled.div<CharBoxProps>`
   background-color: ${(props) => {
     switch (props.$status) {
       case "current":
-        return "#add8e6"; // bleu
+        return "#57D6FE";
       case "correct":
-        return "#00ff00"; // vert
+        return "#AEFD9A";
       case "incorrect":
-        return "#ff0000"; // rouge
+        return "#FD9AA6";
       default:
-        return "#dcfcd4";
+        return "";
     }
   }};
 `;
 
-const SmallBox = styled(CharBox)`
-  width: 8px;
-`;
-
 const TypingBox: React.FC = () => {
+  const [isDeadKey, setIsDeadKey] = useState<boolean>(false);
+  const [deadKeyChar, setDeadKeyChar] = useState<string>("");
+  const [currentCharPosition, setCurrentCharPosition] = useState(0);
+  const [charStatuses, setCharStatuses] = useState<CharStatus>({});
+  const [containerWidth, setContainerWidth] = useState(0);
+  const [lines, setLines] = useState<LineType[]>([]);
+  const loadingStatus = useSelector((state: RootState) => state.texts.status);
+
   const dispatch = useAppDispatch();
   const text = useSelector(selectText);
-  const [currentCharPosition, setCurrentCharPosition] = useState(0);
-  const [charStatuses, setCharStatuses] = useState<Record<number, string>>({});
-
   const typingBoxRef = useRef<HTMLDivElement>(null);
+  const words = text.split(" ");
+  const charMargin = 1;
+  const charWidth = 18 + 2 * charMargin;
+  const lineRefs = useRef<(HTMLSpanElement | null)[]>([]);
+
+  const numberOfCharsPossibleInLine = useMemo(() => {
+    const paddingRem = 1.2 * 16;
+    const usableTypingBoxWidth = containerWidth - 2 * paddingRem;
+    return Math.floor(usableTypingBoxWidth / charWidth);
+  }, [containerWidth, charWidth]);
+
+  const [translateY, setTranslateY] = useState(0);
+  const lineHeight = 49;
+
+  useEffect(() => {
+    if (loadingStatus === "succeeded" && typingBoxRef.current) {
+      typingBoxRef.current.focus();
+    }
+  }, [loadingStatus]);
 
   useEffect(() => {
     dispatch(fetchTexts());
-    if (typingBoxRef.current) {
-      typingBoxRef.current.focus();
-    }
   }, [dispatch]);
 
-  const [accent, setAccent] = useState<string | null>(null);
+  useEffect(() => {
+    const handleResize = () => {
+      if (typingBoxRef.current) {
+        setContainerWidth(typingBoxRef.current.offsetWidth);
+      }
+    };
+    handleResize();
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, [typingBoxRef]);
+
+  useEffect(() => {
+    lineRefs.current = lineRefs.current.slice(0, lines.length);
+  }, [lines]);
+
+  function createNewLine(newLines: LineType[], currentLine: WordWithIndex[]) {
+    if (currentLine.length > 0) {
+      newLines.push({ line: currentLine, lineIndex: newLines.length });
+    }
+  }
+
+  const createLinesAndStartIndices = useCallback(
+    (words: string[], numberOfCharsPossibleInLine: number) => {
+      const newLines: LineType[] = [];
+      let currentLine: WordWithIndex[] = [];
+      let currentLineLength = 0;
+      let charIndex = 0;
+
+      words.forEach((word) => {
+        const wordLength = word.length + 1;
+
+        if (currentLineLength + wordLength <= numberOfCharsPossibleInLine) {
+          currentLine.push({ word, startIndex: charIndex });
+          currentLineLength += wordLength;
+          charIndex += wordLength;
+        } else {
+          createNewLine(newLines, currentLine);
+          currentLine = [{ word, startIndex: charIndex }];
+          currentLineLength = wordLength;
+          charIndex += wordLength;
+        }
+      });
+
+      createNewLine(newLines, currentLine);
+      return newLines;
+    },
+    []
+  );
+
+  useEffect(() => {
+    const newLines = createLinesAndStartIndices(
+      words,
+      numberOfCharsPossibleInLine
+    );
+
+    if (JSON.stringify(newLines) !== JSON.stringify(lines)) {
+      setLines(newLines);
+    }
+  }, [words, numberOfCharsPossibleInLine, lines, createLinesAndStartIndices]);
 
   const handleKeyPress = (event: React.KeyboardEvent) => {
-    event.preventDefault(); // Empêcher les comportements par défaut des touches comme la navigation du navigateur
-    console.log("event.key:", event.key);
-    console.log("event.ctrlKey:", event.ctrlKey);
-
-    // Ignorer l'événement si l'utilisateur a appuyé sur Shift ou Control seulement
-    if (
-      event.key === "Shift" ||
-      event.key === "Control" ||
-      event.ctrlKey ||
-      event.altKey ||
-      event.key === "AltGraph"
-    ) {
+    event.preventDefault();
+    if (event.key === "Dead") {
+      if (isDeadKey) {
+        setDeadKeyChar((prevChar) => prevChar + event.key);
+      } else {
+        setIsDeadKey(true);
+        setDeadKeyChar(event.key);
+      }
       return;
     }
 
     const char = text.charAt(currentCharPosition);
     let expectedKey = char;
+    let newStatus = "incorrect";
 
-    if (accent) {
-      expectedKey = accent + char;
+    if (event.ctrlKey || event.altKey || event.key === "AltGraph") {
+      return;
     }
 
-    if (event.key.length === 1 && RegExp(/[\^¨]/).exec(event.key)) {
-      // Si l'utilisateur a appuyé sur une touche d'accent
-      setAccent(event.key);
-    } else {
-      setAccent(null); // Réinitialiser l'accent pour les futurs caractères
-      if (event.shiftKey) {
-        expectedKey = expectedKey.toUpperCase();
-      }
-
-      const newStatus = event.key === expectedKey ? "correct" : "incorrect";
-      setCharStatuses({
-        ...charStatuses,
-        [currentCharPosition]: newStatus,
-      });
-      setCurrentCharPosition((prev) => prev + 1);
+    if (event.key === char || event.key === char.toUpperCase()) {
+      newStatus = "correct";
     }
-  };
-  const words = text.split(" ");
+    if (isDeadKey) {
+      const combinedChar = deadKeyChar + event.key;
+      expectedKey = combinedChar;
+      setIsDeadKey(false);
+      setDeadKeyChar("");
+    }
 
-  const handleBoxSizing = (char: string, status: string | null) => {
-    switch (char) {
-      case ".":
-      case ",":
-      case ";":
-      case ":":
-      case "!":
-      case "'":
-      case "i":
-      case "l":
-      case "I":
-      case "t":
-      case "j":
-        return (
-          <SmallBox key={uuidv4()} $status={status}>
-            {char}
-          </SmallBox>
-        );
-      default:
-        return (
-          <CharBox key={uuidv4()} $status={status}>
-            {char}
-          </CharBox>
-        );
+    if (event.key === "Shift" || event.key === "CapsLock") {
+      return;
+    }
+
+    if (event.key === expectedKey) {
+      newStatus = "correct";
+    }
+
+    setCharStatuses({
+      ...charStatuses,
+      [currentCharPosition]: newStatus,
+    });
+    setCurrentCharPosition(currentCharPosition + 1);
+
+   
+    const nextCharBox = typingBoxRef.current?.querySelector(`[data-charindex="${currentCharPosition + 1}"]`);
+    if (nextCharBox && nextCharBox.getAttribute('data-firstchar') === 'true') {
+      setTranslateY(prevTranslateY => prevTranslateY + lineHeight); // Augmentez translateY de 49px
     }
   };
-  interface Accumulator {
-    elements: JSX.Element[];
-    absoluteCharIndex: number;
-  }
+
+  const getCharStatus = (charIndex: number): string | null => {
+    if (charIndex === currentCharPosition) {
+      return "current";
+    }
+    return charStatuses[charIndex] || null;
+  };
+
+  const wordSplitter = (
+    word: string,
+    startIndex: number,
+    wordIndex: number,
+    lineIndex: number,
+    isLastWordInText: boolean
+  ): JSX.Element[] => {
+    const chars = word.split("").map((char, charIndex) => {
+      const absoluteCharIndex = startIndex + charIndex;
+      const charStatus = getCharStatus(absoluteCharIndex);
+      const isFirstChar = lineIndex >= 6 && lineIndex < lines.length - 2&& wordIndex === 0 && charIndex === 0;
+  
+      return (
+        <CharBox
+          key={`char-${lineIndex}-${wordIndex}-${charIndex}`}
+          $status={charStatus}
+          $charIndex={charIndex}
+          $wordIndex={wordIndex}
+          $lineIndex={lineIndex}
+          data-charindex={absoluteCharIndex}
+          {...(isFirstChar ? { "data-firstchar": "true" } : {})}
+        >
+          {char}
+        </CharBox>
+      );
+    });
+  
+    if (!isLastWordInText) {
+      chars.push(
+        <CharBox
+          key={`space-${lineIndex}-${wordIndex}`}
+          $lineIndex={lineIndex}
+          $wordIndex={wordIndex}
+          $charIndex={word.length}
+          $status={getCharStatus(startIndex + word.length)}
+        />
+      );
+    }
+
+    return chars;
+  };
+
   return (
     <TypingBoxContainer
       ref={typingBoxRef}
       onKeyDown={handleKeyPress}
       tabIndex={0}
     >
-      {
-        words.reduce<Accumulator>(
-          (acc, word, index) => {
-            const characters = word.split("");
-            const elements = [
-              ...acc.elements,
-              <React.Fragment key={uuidv4()}>
-                <WordContainer>
-                  {characters.map((char, charIndex) => {
-                    const absoluteCharIndex = acc.absoluteCharIndex + charIndex;
-                    const status =
-                      charStatuses[absoluteCharIndex] ||
-                      (absoluteCharIndex === currentCharPosition
-                        ? "current"
-                        : null);
-                    return handleBoxSizing(char, status);
-                  })}
-                </WordContainer>
-                {index !== words.length - 1 && (
-                  <>
-                    <SmallBox
-                      $status={
-                        charStatuses[acc.absoluteCharIndex + word.length] ||
-                        (acc.absoluteCharIndex + word.length ===
-                        currentCharPosition
-                          ? "current"
-                          : null)
-                      }
-                    />
-                  </>
+      <TextContainer $translateY={translateY}>
+        {lines.map(({ line, lineIndex }, lineArrayIndex, linesArray) => (
+          <LineContainer
+            className="line"
+            key={`line-${lineIndex}`}
+            ref={(el) => (lineRefs.current[lineIndex] = el)}
+          >
+            {line.map(({ word, startIndex }, wordIndex, wordArray) => (
+              <WordContainer key={`word-${lineIndex}-${wordIndex}`}>
+                {wordSplitter(
+                  word,
+                  startIndex,
+                  wordIndex,
+                  lineIndex,
+                  lineArrayIndex === linesArray.length - 1 &&
+                    wordIndex === wordArray.length - 1 // Ajoutez ce paramètre ici
                 )}
-              </React.Fragment>,
-            ];
-            return {
-              elements: elements,
-              absoluteCharIndex: acc.absoluteCharIndex + word.length + 1, // +1 pour l'espace
-            };
-          },
-          { elements: [], absoluteCharIndex: 0 }
-        ).elements
-      }
+              </WordContainer>
+            ))}
+          </LineContainer>
+        ))}
+      </TextContainer>
     </TypingBoxContainer>
   );
 };
